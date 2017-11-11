96cd15fb222587fd500cb992e0d36ddfceabe6fc
Chad <cdhqwebdev@gmail.com>
2017-11-10 19:55:54 -0500

    add client side config

--- services/passport.js
+++ services/passport.js
@@ -6,32 +6,31 @@ const keys = require("../config/keys");
 const User = mongoose.model("user");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-     .then(user => {
-         done(null, user);
-     });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback",
-    proxy:true
- }, (accessToken, refreshToken, profile, done) => {
-       User.findOne({ googleID: profile.id}).then((existingUser) => {
-             if (existingUser) {
-                 done(null, existingUser );
-             } else {
-               new User({ googleID: profile.id})
-                 .save()
-                 .then(user => done(null, user));
-             }
-         });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
     }
-   )
- );
+  )
+);
