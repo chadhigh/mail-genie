998de5770edca5bf862f7718e604814c36cfbaf9
Chad <cdhqwebdev@gmail.com>
2017-11-18 17:14:43 -0500

    edit survey form and fields, add email validation

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>
+  return <div className="container" style={{textAlign: 'center'}}>
   <h4>Dashboard</h4>
   <div className="fixed-action-btn">
-    <Link to="/surveys/new"className="btn-floating btn-large blue">
+    <Link to="/surveys/new"className="btn-floating btn-large right blue">
       <i className="large material-icons">add</i>
     </Link>
   </div>
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,47 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title", noValueError: "*must provide a survey title*" },
+  { label: "Subject Line", name: "subject", noValueError: "*must provide a subject*" },
+  { label: "Email Body", name: "body", noValueError: "*must provide a body*" },
+  { label: "Recipient List", name: "emails", noValueError: "*must provide emails*" }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
   render() {
     return (
-      <div className="container" style={{textAlign: 'center'}}>
+      <div
+        className="container"
+        style={{ textAlign: "center", paddingTop: "50px" }}
+      >
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderFields()}
+          <Link to="/surveys" className="btn-floating btn-large left red">
+            <i className="material-icons left">close</i>
+          </Link>
           <button
-            className="btn waves-effect waves-light"
+            className="btn-floating btn-large right green"
             type="submit"
             name="action"
-            style={{textAlign: "center"}}
-          >
-            Submit <i className="material-icons right">send</i>
+          ><i className="material-icons right">done</i>
           </button>
         </form>
       </div>
@@ -21,6 +49,21 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
+  
+  _.each(FIELDS, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
+
+
+  return errors;
+}
 export default reduxForm({
+  validate,
   form: "surveyForm"
 })(SurveyForm);
